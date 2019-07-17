73a4286b2cbf55709b8a75a182ec8e36d250c331
Manish Kumawat <30183799+BurningTiles@users.noreply.github.com>
2019-07-17 23:47:30 +0530

    Update LexicalAnalyzerAdvanced.cpp

--- System Programming/LexicalAnalyzerAdvanced.cpp
+++ System Programming/LexicalAnalyzerAdvanced.cpp
@@ -3,7 +3,8 @@
  * It is for source code written in c language.
  * I've tried to make it as good as real.
  * I know there are too many exeptions, and maybe it'll be solved later when i've free time.
- * It will not work properly in windows, an unknown issue of infinite loop.
+ * This program is independent of os.
+ * Maybe it will not work properly in windows, an unknown issue of infinite loop (Windows is shit).
  * In linux for me it works perfectly.
  */
 
