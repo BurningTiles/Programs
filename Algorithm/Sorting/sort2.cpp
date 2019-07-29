380b2b3b28d487660d0431ef94b25da3337f2a45
BurningTiles <30183799+BurningTiles@users.noreply.github.com>
2019-07-29 16:24:35 +0530

    Program to sort list of cities in file input1.txt

---
+++ Algorithm/Sorting/sort2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include "sort.h"
+using namespace std;
+
+int main(){
+	ifstream in("input1.txt");
+	ofstream out("output1.txt");
+
+	if (in.fail()){
+		cerr<<"Error in openning input file."<<endl;
+		return -1;
+	}
+	if (out.fail()){
+		cerr<<"Error in creating output file."<<endl;
+		return -2;
+	}
+
+	int n;
+	if ( ! (in >> n)) {
+		cerr << "Reading failed!\n";
+		return -3;
+	}
+
+	if (n<1){
+		cerr<<"Sorting not possible on less than 1 element."<<endl;
+		return -4;
+	}
+
+	string x[n];
+	for (int i=0; i<n; i++){
+		if(!(in>>x[i])){
+			cerr<<"Error in reading."<<endl;
+			return -5;
+		}
+	}
+
+	quickSort(x,n);
+
+	for (int i=0; i<n; i++)
+		out<<x[i]<<"\n";
+
+	return 0;
+}
