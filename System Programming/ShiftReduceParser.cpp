db095af0785b46a51ecae0d982791f7143c19996
Manish Kumawat <30183799+BurningTiles@users.noreply.github.com>
2019-08-28 22:16:40 +0530

    Shift Reduce Parser program.

--- System Programming/ShiftReduceParser.cpp
+++ System Programming/ShiftReduceParser.cpp
@@ -17,21 +17,22 @@ int top=0;
 string inputString;
 
 bool save(){
-	if (inputString.length()<3){
+
+	if ( inputString.length()<3 ){
 		cerr<<"Not a equation."<<endl;
 		return false;
 	}
-	else if(inputString.at(1)=='-' && inputString.at(2)=='>'){
+	else if( inputString.at(1)=='-' && inputString.at(2)=='>' ){
 		int breakPoint=3;
-		for (int i=2; i<inputString.length(); i++)
-			if (inputString.at(i)=='|'){
-				rightSide[top]=inputString.substr(breakPoint,i-breakPoint);
-				leftNT[top]=inputString.at(0);
-				breakPoint=i+1;
+		for ( int i=2 ; i<inputString.length() ; i++ )
+			if ( inputString.at(i)=='|' ){
+				rightSide[top] = inputString.substr(breakPoint,i-breakPoint);
+				leftNT[top] = inputString.at(0);
+				breakPoint = i+1;
 				top++;
 			}
-		rightSide[top]=inputString.substr(breakPoint,inputString.length());
-		leftNT[top]=inputString.at(0);
+		rightSide[top] = inputString.substr(breakPoint,inputString.length());
+		leftNT[top] = inputString.at(0);
 		top++;
 		return true;
 	}
@@ -42,21 +43,21 @@ bool save(){
 }
 
 void print(){
-	string stack="$";
+	string stack = "$";
 	cout<<endl;
 	cout<<"Stack                    Input                    Action"<<endl;
 	cout<<"-----                    -----                    ------"<<endl;
 	do{
 		int breakPoint;
-		int match=-1;
-		bool shift=true;
-		for (int i=stack.length(); i>1; i--){
-			string temp=stack.substr(i-1,stack.length()-i+1);
-			for (int j=0; j<top; j++){
-				if (temp==rightSide[j]){
-					shift=false;
-					match=j;
-					breakPoint=i-1;
+		int match = -1;
+		bool shift = true;
+		for ( int i=stack.length() ; i>1 ; i-- ){
+			string temp = stack.substr(i-1,stack.length()-i+1);
+			for ( int j=0 ; j<top ; j++ ){
+				if ( temp==rightSide[j] ){
+					shift = false;
+					match = j;
+					breakPoint = i-1;
 					break;
 				}
 			}
@@ -74,11 +75,11 @@ void print(){
 			inputString = inputString.substr(1,inputString.length());
 		}
 		
-	} while (inputString!="");
+	} while ( inputString!="" );
 	
 	stack.pop_back();
 	
-	if(stack.length()==2 && stack.at(1)==leftNT[0])
+	if( stack.length()==2 && stack.at(1)==leftNT[0] )
 		cout<<endl<<endl<<"Accepted..."<<endl<<endl;
 	else
 		cout<<endl<<endl<<"Rejected..."<<endl<<endl;
@@ -87,12 +88,12 @@ void print(){
 int main(){
 	cout<<"When you're done enter q.\nEnter equations : "<<endl;
 	cin>>inputString;
-	while (inputString!="q"){
+	while ( inputString!="q" ){
 		if(!save()) return -1;
 		cin>>inputString;
 	}
 	cout<<endl<<"Enter input string : ";
-	if (top==0) {
+	if ( top==0 ) {
 		cout<<"No equations found."<<endl;
 		return -2;
 	}
