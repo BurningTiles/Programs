1fbc72119e2a3283aff9fbe21608358c14cd5c2b
Manish Kumawat <30183799+BurningTiles@users.noreply.github.com>
2020-07-14 01:16:11 +0530

    New Added

---
+++ Other/CodeQuotient/Day2/DistributeBook.cpp
@@ -0,0 +1,65 @@
+/*
+ * Author : BurningTiles
+ */
+
+#include <iostream>
+#define ll long long
+using namespace std;
+
+int main(){
+    ll x[1000001]={1,0,1},n;
+    cin >> n;
+    if(n<3){
+        cout << x[n];
+        return 0;
+    }
+    for(ll i=3; i<=n; i++)
+        x[i] = (i-1)*(x[i-1]+x[i-2])%100000007;
+    cout << x[n];
+    return 0;
+}
+
+
+/*
+For enhancing the book reading, school distributed story books to students as part of the Children’s day celebrations. To increase the reading habit, the class teacher decided to exchange the books every weeks so that everyone will have a different book to read. She wants to know how many possible exchanges are possible.
+
+If they have 4 books and students, the possible exchanges are 9. Bi is the book of i-th student and after the exchange, he should get a different book, other than Bi.
+
+B1 B2 B3 B4 – first state, before exchange of the books
+
+B2 B1 B4 B3
+
+B2 B3 B4 B1
+
+B2 B4 B1 B3
+
+B3 B1 B4 B2
+
+B3 B4 B1 B2
+
+B3 B4 B2 B1
+
+B4 B1 B2 B3
+
+B4 B3 B1 B2
+
+B4 B3 B2 B1
+
+Find the number of possible exchanges, if the books are exchanged so that every student will receive a different book.
+
+
+Constraints
+1<= N <= 1000000
+
+Input Format
+Input contains one line with N, indicates the number of books and number of students.
+
+Output Format
+Output the answer modulo 100000007.
+
+Sample Input
+4
+
+Sample Output
+9
+*/
